8f1dc8f7daddf70c79a775169fc4982b4ce88af5
Mario Nebl <marionebl@users.noreply.github.com>
2017-08-26 19:54:03 +0200

    Update npm.go

--- src/nodejs/npm/npm.go
+++ src/nodejs/npm/npm.go
@@ -2,9 +2,7 @@ package npm
 
 import (
 	"io"
-	"os"
 	"path/filepath"
-
 	"github.com/cloudfoundry/libbuildpack"
 )
 
@@ -19,23 +17,13 @@ type NPM struct {
 }
 
 func (n *NPM) Build() error {
-	doBuild, source, err := n.doBuild()
-	if err != nil {
-		return err
-	}
-	if !doBuild {
-		return nil
-	}
-
-	n.Log.Info("Installing node modules (%s)", source)
-	npmArgs := []string{"install", "--unsafe-perm", "--userconfig", filepath.Join(n.BuildDir, ".npmrc"), "--cache", filepath.Join(n.BuildDir, ".npm")}
-	return n.Command.Execute(n.BuildDir, n.Log.Output(), n.Log.Output(), "npm", npmArgs...)
+	return n.Command.Execute(n.BuildDir, n.Log.Output(), n.Log.Output(), "echo", "'skipping'")
 }
 
 func (n *NPM) Rebuild() error {
-	return n.Command.Execute(n.BuildDir, n.Log.Output(), n.Log.Output(), "echo", []string{"'skipping'"})
+	return n.Command.Execute(n.BuildDir, n.Log.Output(), n.Log.Output(), "echo", "'skipping'")
 }
 
 func (n *NPM) doBuild() (bool, string, error) {
-	return n.Command.Execute(n.BuildDir, n.Log.Output(), n.Log.Output(), "echo", []string{"'skipping'"})
+	return n.Command.Execute(n.BuildDir, n.Log.Output(), n.Log.Output(), "echo", "'skipping'")
 }
