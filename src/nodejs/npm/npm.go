edea52785d023b84f909e37641c941ee07672294
Mario Nebl <marionebl@users.noreply.github.com>
2017-08-27 21:23:15 +0200

    fix: skip actual work

--- src/nodejs/npm/npm.go
+++ src/nodejs/npm/npm.go
@@ -2,6 +2,9 @@ package npm
 
 import (
 	"io"
+	"os"
+	"path/filepath"
+
 	"github.com/cloudfoundry/libbuildpack"
 )
 
@@ -16,16 +19,56 @@ type NPM struct {
 }
 
 func (n *NPM) Build() error {
-	args := []string{"'skipping'"}
-	return n.Command.Execute(n.BuildDir, n.Log.Output(), n.Log.Output(), "echo", args...)
+	doBuild, source, err := n.doBuild()
+	if err != nil {
+		return err
+	}
+	if !doBuild {
+		return nil
+	}
+
+	n.Log.Info("Installing node modules (%s)", source)
+	npmArgs := []string{"install", "--unsafe-perm", "--userconfig", filepath.Join(n.BuildDir, ".npmrc"), "--cache", filepath.Join(n.BuildDir, ".npm")}
+	return n.Command.Execute(n.BuildDir, n.Log.Output(), n.Log.Output(), "echo", npmArgs...)
 }
 
 func (n *NPM) Rebuild() error {
-	args := []string{"'skipping'"}
-	return n.Command.Execute(n.BuildDir, n.Log.Output(), n.Log.Output(), "echo", args...)
+	doBuild, source, err := n.doBuild()
+	if err != nil {
+		return err
+	}
+	if !doBuild {
+		return nil
+	}
+
+	n.Log.Info("Rebuilding any native modules")
+	if err := n.Command.Execute(n.BuildDir, n.Log.Output(), n.Log.Output(), "npm", "rebuild", "--nodedir="+os.Getenv("NODE_HOME")); err != nil {
+		return err
+	}
+
+	n.Log.Info("Installing any new modules (%s)", source)
+	npmArgs := []string{"install", "--unsafe-perm", "--userconfig", filepath.Join(n.BuildDir, ".npmrc")}
+	return n.Command.Execute(n.BuildDir, n.Log.Output(), n.Log.Output(), "echo", npmArgs...)
 }
 
 func (n *NPM) doBuild() (bool, string, error) {
-	args := []string{"'skipping'"}
-	return n.Command.Execute(n.BuildDir, n.Log.Output(), n.Log.Output(), "echo", args...)
+	pkgExists, err := libbuildpack.FileExists(filepath.Join(n.BuildDir, "package.json"))
+	if err != nil {
+		return false, "", err
+	}
+
+	if !pkgExists {
+		n.Log.Info("Skipping (no package.json)")
+		return false, "", nil
+	}
+
+	shrinkwrapExists, err := libbuildpack.FileExists(filepath.Join(n.BuildDir, "npm-shrinkwrap.json"))
+	if err != nil {
+		return false, "", err
+	}
+
+	if shrinkwrapExists {
+		return true, "package.json + shrinkwrap", nil
+	}
+	return true, "package.json", nil
 }
